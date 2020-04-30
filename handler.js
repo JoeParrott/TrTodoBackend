135f651190e2d1978a1c382fb2e6aab7ae57a2ab
JoeParrott <JosephParrott1@outlook.com>
2020-04-30 20:32:55 +0100

    lamba communicating with express and sql now, still have a server error msg coming back from post but task is being created, still debugging and finishing escapes

--- handler.js
+++ handler.js
@@ -7,73 +7,82 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 require('dotenv').config();
 
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const pw = process.env.DB_PASSWORD;
+const database = process.env.DB_DATABASE;
+
 const dbConnection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+  host,
+  user,
+  password: pw,
+  database,
 });
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-//task view
-app.get('/tasks', function (request, response) {
-    dbConnection.query(`SELECT * FROM TaskList`, (err, data) => {
-        if (err) {
-            console.log('Error from DB', err);
-            response.status(500).send(err)
-        } else {
-            response.status(200).send(data);
-        }
+// task view
+
+app.get('/tasks', (request, response) => {
+  dbConnection.query('SELECT * FROM TaskList', (err, data) => {
+    if (err) {
+      console.log('Error from DB', err);
+      response.status(500).send(err);
+    } else {
+      response.status(200).send(data);
+    }
+  });
+});
+
+// task update
+app.put('/tasks/:id', (request, response) => {
+  const id = request.params.id;
+  const data = request.body; // need to think through how a change request will come in!
+  dbConnection.query('UPDATE TaskList SET ? WHERE TaskID = ?', [data, id], (err) => {
+    if (err) {
+      console.log('Error from DB', err);
+      response.status(500).send(err);
+    } else {
+      response.status(201).send(`Successfully updated task${id}`);
+    }
+  });
+});
+
+// new task
+app.post('/tasks', (request, response) => {
+  const data = request.body;
+  dbConnection.query('INSERT INTO TaskList (Title, Description, Completed, DueDate) VALUES (?, ?, ?, ?)',
+    [data.Title, data.Description, false, data.DueDate], (err, results) => {
+      if (err) {
+        console.log('Error from DB', err);
+        response.status(500).send(err);
+      } else {
+        dbConnection.query(`SELECT * FROM TaskList WHERE TaskID = ${results.insertId}`, (error, results) => {
+          if (error) {
+            console.log('Error from DB', error);
+            response.status(500).send(error);
+          } else {
+            response.status(201).send(results[0], `Successfully created task ${results.insertId}`);
+          }
+        });
+      }
     });
 });
 
-//task update
-app.put('/tasks/:id', (request, response) => {
-    const id = request.params.id;
-    const data = request.body ; //need to think through how a change request will come in!
-    /*dbConnection.query(`UPDATE TaskList SET ${data.key}=${data.value} WHERE TaskID=${id}`, function(err, data) {
-        if (err) {
-            console.log('Error from DB', err);
-            response.status(500).send(err)
-        } else {
-            response.status(201).send(data);
-        } 
-    })*/
-});
-
-//newtask
-app.post('/tasks', (request, response) => {
-    const data = request.body;
-    dbConnection.query(`INSERT INTO TaskList (Title, Description, Completed, DueDate) VALUES (?, ?, ?, ?)`, [data.Title, data.Description, FALSE, data.DueDate], (err, results) => {
-        if (err) {
-            console.log('Error from DB', err);
-            response.status(500).send(err)
-        } else {
-            dbConnection.query(`SELECT * FROM TaskList WHERE TaskID = ${results.insertId}`, (err, results) => {
-                  if (err) {
-                    console.log('Error from DB', err);
-                    response.status(500).send(err);
-                  } else {
-                    response.status(201).send(results[0]);
-                  }
-                }
-              ); 
-            };
-        });
-});
-
-//task delete
-app.delete('/tasks/:id', (err, data) => {
-    const id = request.params.id;
-    dbConnection.query(`DELETE FROM TaskList WHERE TaskID=${id}`, (err, data) => {
+// task delete
+app.delete('/tasks/:id', (request, response) => {
+  const id = request.params.id;
+  // need security on the /:id user-input
+  dbConnection.query(`DELETE FROM TaskList WHERE TaskID=${id}`, (err, data) => {
     if (err) {
-        console.log('Error from DB', err);
-        response.status(500).send(err)
+      console.log('Error from DB', err);
+      response.status(500).send(err);
     } else {
-        response.status(200).send(`Task ${id} successfully deleted.`);
+      response.status(200).send(`Task ${id} successfully deleted.`);
     }
+  });
 });
 
 module.exports.app = serverlessHttp(app);
